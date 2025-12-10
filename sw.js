e7e35f4136b5642212a2a90626798ddd7e97d15e
erfara2009 <erfara29@gmail.com>
2025-12-10 21:56:19 +0300

    Initial commit: overwrite repo with updated files

---
+++ sw.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  // SPA fallback: serve index.html for navigation requests
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      fetch(event.request).catch(() => caches.match('/index.html'))
+    );
+  }
+});
